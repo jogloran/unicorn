42d0f684b1a4f51ce0bd5e3cb3dcf67f7cec1872
Phil Howard <github@gadgetoid.com>
2026-08-29 11:42:07 +0100

    CI: Move Python linting to ruff.
    flake8 with --ignore E501 caught little beyond pyflakes. Take plasma's
    ci/ruff.toml and ci/python.sh, which lint modules/ as well as examples/,
    and pin ruff.

---
+++ ci/python.sh
@@ -0,0 +1,31 @@
+QA_SCRIPT_PATH=${BASH_SOURCE-$0}
+QA_SCRIPT_PATH=$(dirname "$QA_SCRIPT_PATH")
+QA_SCRIPT_PATH=$(realpath "$QA_SCRIPT_PATH")
+
+function qa_prepare_all {
+    pip install -r "$QA_SCRIPT_PATH/requirements-qa.txt"
+}
+
+function qa_check {
+    ruff check --config "$QA_SCRIPT_PATH/ruff.toml" "$1"
+}
+
+function qa_fix {
+    ruff check --config "$QA_SCRIPT_PATH/ruff.toml" --fix "$1"
+}
+
+function qa_examples_check {
+    qa_check examples/
+}
+
+function qa_examples_fix {
+    qa_fix examples/
+}
+
+function qa_modules_check {
+    qa_check modules/
+}
+
+function qa_modules_fix {
+    qa_fix modules/
+}
